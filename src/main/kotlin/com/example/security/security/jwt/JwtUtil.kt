c404f0ead2a2a3cb32972e032eaa83cb12985896
Christoffer <links685@gmail.com>
2022-03-25 23:58:30 +0100

    extracted all insecurities from source except test users. Also runs on Heroku!

--- src/main/kotlin/com/example/security/security/jwt/JwtUtil.kt
+++ src/main/kotlin/com/example/security/security/jwt/JwtUtil.kt
@@ -3,12 +3,27 @@ package com.example.security.security.jwt
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.interfaces.DecodedJWT
+import org.jboss.logging.Logger
+import org.springframework.boot.CommandLineRunner
 import org.springframework.security.core.userdetails.User
+import org.springframework.stereotype.Component
 import java.util.*
 
+private var cookieSecret: String? = null
+
+@Component
+class CookieSecretInitializer: CommandLineRunner {
+    override fun run(vararg args: String?) {
+        cookieSecret = System.getenv()["COOKIE_SECRET"]
+        if (cookieSecret.isNullOrBlank())
+            throw RuntimeException("COOKIE_SECRET env variable must be set")
+    }
+
+}
+
+
 object JwtUtil {
-    private const val SECRET = "do_not_store_secrets_in_the_source_code"
-    private val algorithm = Algorithm.HMAC256(SECRET)
+    private val algorithm = Algorithm.HMAC256(cookieSecret)
 
     fun createToken(user: User, issuer: String, minutes: Int): String {
         return JWT.create()
