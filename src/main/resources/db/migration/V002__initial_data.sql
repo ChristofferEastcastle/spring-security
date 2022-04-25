c4b11941105ad13643da70554c4d5deb1c07ac9f
Christoffer <links685@gmail.com>
2022-04-25 23:59:38 +0200

    added password comments to migration

--- src/main/resources/db/migration/V002__initial_data.sql
+++ src/main/resources/db/migration/V002__initial_data.sql
@@ -15,10 +15,13 @@ values (default, 'ADMIN'),
 insert into users
 values (default, 'administrator',
         '$2a$10$mJSDNY7ZmiAaZzbHfgosfOeoK56je/opq34Z1ff7KxHJs/Gtk8mra', 'true'),
+       -- password = 'super_secret'
        (default, 'regular_user',
-        '$2a$12$9/BylAxHWgpLymIECTZQreEIwKktf2/.f39e3pP6Rwcw2ICOUjXRq', 'true'),
+        '$2a$12$xJo98SXGhxI390SiVP4tcePqh1Ga4qJnssjIT9J6UHkSMEuTDL2Yi', 'true'),
+       -- password = 'best_password'
        (default, 'useless_trainee',
-        '$2a$12$6tfi0tvxpigIoOJuibWbzOoo0DgtjDt9gfF9oMZjdKwKUcx.vNttu', 'true');
+        '$2a$12$iA055Rpzx9shrJGBp92qYeFe9mlP2XVdB1dlrOXPC9GcNlXp8o1NW', 'true');
+       -- password = 'how_to_make_password'
 
 insert into users_authorities
 values ((select id from users where username = 'administrator'),
